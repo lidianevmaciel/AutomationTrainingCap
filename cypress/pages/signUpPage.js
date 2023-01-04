b7e5c88f32b80c7411602b0c933488118b5246a3
Maciel <lidiane.maciel@capgemini.com>
2023-01-04 19:57:14 -0300

    Mapeando Address information

--- cypress/pages/signUpPage.js
+++ cypress/pages/signUpPage.js
@@ -1,13 +1,21 @@
 class signUpPage {
 
     elements = {
+        //ENTER ACCOUNT INFORMATION
         inputName: () => cy.get('#name'),
         inputEmail: () => cy.get('#email'),
         inputPasswd: () => cy.get('#password'),
         inputFirstName: () => cy.get('#first_name'),
         inputLastName: () => cy.get('#last_name'),
+
+        //ADDRESS INFORMATION
         inputAddress: () => cy.get('#address1'),
-        selectCountry: () => cy.get('#country')
+        selectCountry: () => cy.get('#country'),
+        inputState: () => cy.get('#state'), 
+        inputcity: () => cy.get('#city'),
+        inputZipCode: () => cy.get('#zipcode'), 
+        inputMobileNumber: () => cy.get('#mobile_number')
+
     }
 
     selectCountryOption() {
